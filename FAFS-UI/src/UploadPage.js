e0e3c93a213dd998ab5325785d2cfdc15ce2a8c4
hbeach893 <hannahbeach983@hannahs-mbp-4.middlebury.edu>
2017-12-03 14:41:12 -0500

    small UI changeS

--- FAFS-UI/src/UploadPage.js
+++ FAFS-UI/src/UploadPage.js
@@ -14,16 +14,18 @@ export default class UploadPage extends Component {
 		return (
 			<View style={styles.container}>
 				 <NavBar navigator={this.props.navigator}/>
-					<Button onPress={() => this.props.navigator.push({name: 'upload_inventory'})}
-						color="navy"
-						title="Upload an Item"
-						accessibilityLabel="View Rideshare Page"
-				    />
-		          <Button onPress={() => {this.props.navigator.push({name: 'upload_ride'})}}
-			            color="navy"
-			            title="Upload Ride"
-			            accessibilityLabel="View Rideshare Page"
-		          />
+				 	<View style={styles.buttons}>
+						<Button onPress={() => this.props.navigator.push({name: 'upload_inventory'})}
+							color="navy"
+							title="Upload an Item"
+							accessibilityLabel="View Rideshare Page"
+						/>
+						<Button onPress={() => {this.props.navigator.push({name: 'upload_ride'})}}
+						    color="navy"
+						    title="Upload a Ride"
+						    accessibilityLabel="View Rideshare Page"
+						/>
+					</View>
 			</View>
 			)
 	}
@@ -35,5 +37,9 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 30,         // start below status bar
   },
+
+  buttons: {
+  	paddingTop: 20,
+  }
 
 });
